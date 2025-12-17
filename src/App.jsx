d9fb457d81ddfb961c07cf0c59011e6259dcf6c5
suspicious115478 <sus24picious45@gmail.com>
2025-12-17 15:45:58 +0530

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AgentsTable from './components/AgentsTable';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Agents Dashboard</h1>
+      <AgentsTable />
+    </div>
+  );
+}
+
+export default App;
